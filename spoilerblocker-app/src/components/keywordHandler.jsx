a757028a9a52c876945e694966affe09081f1b8f
Alex <voynikovalex12@gmail.com>
2026-06-21 16:19:06 +0100

    Removed unnecessary files that were originally made when react was first imported. Set up the basics components I will need for my DOM environment.

---
+++ spoilerblocker-app/src/components/keywordHandler.jsx
@@ -0,0 +1,15 @@
+function KeyWordHandler(){
+    return <div>
+        <h1>Test</h1>
+        <div className="searchBar">
+            <h1>Keyword enterer</h1>
+            <input placeholder="Enter Text"></input>
+        </div>
+
+        <div className="submissionHandler">
+
+        </div>
+    </div>
+}
+
+export default KeyWordHandler;
---
+++ spoilerblocker-app/src/components/mainPage.jsx
@@ -0,0 +1,8 @@
+import "../Css/mainPage.css"
+import KeyWordHandler from "./keywordHandler";
+
+function MainPage(){
+    return <div> <KeyWordHandler/> </div>
+}
+
+export default MainPage;
