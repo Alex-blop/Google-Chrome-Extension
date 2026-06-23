e58d27ae7d40d53a3b514d50c0324074fb569069
Alex <voynikovalex12@gmail.com>
2026-06-23 20:27:07 +0100

    Made it so users are able to add and remove specific keywords. these keywords are currently being stored in an array list that i can append to and remove from.

--- spoilerblocker-app/src/components/keywordHandler.jsx
+++ spoilerblocker-app/src/components/keywordHandler.jsx
@@ -1,21 +1,52 @@
-function KeyWordHolder(){
-    const keywords = []
-    return <div>
-        <h1>Test</h1>
-        <div className="searchBar">
-            <h1>Keyword enterer</h1>
-            <input id="EnteredValue" placeholder="Enter Text" type="text"></input>
-        </div>
+import { useState } from "react";
 
-        <div className="submissionHandler">
-            <button onClick={KeyWordHandler}>Submit</button>
-        </div>
+function KeyWordHolder() {
+  const [Keywords, setKeywords] = useState([]);
+  const [newKeyword, setNewKeyword] = useState("");
+
+  const inputKeyword = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const SubmmitedKeywords = formData.get("keywords");
+
+    if (SubmmitedKeywords && SubmmitedKeywords.trim() !== "") {
+      setKeywords([...Keywords, SubmmitedKeywords]);
+      setNewKeyword(SubmmitedKeywords);
+    }
+    e.target.reset();
+  };
+
+  const deleteKeywordHandler = (indexToDelete) => {
+    const updatedKeywords = Keywords.filter(
+      (_, index) => index !== indexToDelete,
+    );
+    setKeywords(updatedKeywords);
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={inputKeyword}>
+          <h1>Enter Keywords</h1>
+          <input name="keywords" placeholder="Enter Keywords" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div>
+        <h1>Current Keywords</h1>
+        <ul>
+          {Keywords.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => deleteKeywordHandler(index)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
+  );
 }
-
-function KeyWordHandler(){
-    let input = document.getElementById("EnteredValue")
-    alert(input)
-}
-
 export default KeyWordHolder;
